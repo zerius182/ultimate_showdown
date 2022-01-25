adad844facf5d52dcc917a75120c45acceca77a3
zerius182 <garryjoynes182@hotmail.co.uk>
2022-01-25 12:59:00 +0000

    Added functions to play sound effects

--- scripts/game.js
+++ scripts/game.js
@@ -7,18 +7,21 @@ const villianMoves = ["shield", "wand", "sword"];
 
 function shieldHit(){
     playerAttack = "shield";
+    shieldSound.play();
     villainRoll();
     attackPhase();
 }
 
 function wandHit(){
     playerAttack = "wand";
+    wandSound.play();
     villainRoll();
     attackPhase();
 }
 
 function swordHit(){
     playerAttack = "sword";
+    swordSound.play();
     villainRoll();
     attackPhase();
 }
@@ -57,31 +60,38 @@ function resolveAttacks(playerAttack, villainAttack){
     else if (playerAttack === "shield" && villainAttack === "sword"){
         result = "The villain lunges at you swinging his sword in a wild arc, you raise your shield deflecting the blow, causing the villain to stumble which you take advantage off with a huge blow to his head with your shield, causing a crushing wound to your opponent.";
         victor = "player";
+        swordSound.play();
     }
 
     else if (playerAttack === "wand" && villainAttack === "shield"){
         result = "You instantly unleash a huge barrage of magic at your opponent before he has time to counter with a spell of his own, desperate, he raises his shield to minimise the imapct of the blast, which saves his life but the villain is still badly wounded.";
         victor = "player";
+        shieldSound.play();
     }
 
     else if (playerAttack === "sword" && villainAttack === "wand"){
         result = "Your opponent aims a blast of magic towards you which you roll under whilst unsheathing your sword. As the blast passes over your head you swing your sword and score a hit on your oppent causing a grievious wound.";
         victor = "player";
+        wandSound.play();
     }
+
 
     else if (playerAttack === "sword" && villainAttack === "shield"){
         result = "You run towards your opponent, trying to catch him off guard and deliver a blow with your sword but he is prepared, raising his shield and deflecting your attack before bringing the shield down on your head with tremendous force, causing you to be brutally wounded. ";
         victor = "villain";
+        shieldSound.play();
     }
 
     else if (playerAttack === "shield" && villainAttack === "wand"){
         result = "You notice your opponent readying an attack with his sword and raise your shield in preperation to deflect the blow but at the last moment he unleashes a huge bolt at magic at you which your shield only partially deflects, causing you to suffer a horrendous wound.";
         victor = "villain";
+        wandSound.play();
     }
 
     else if (playerAttack === "wand" && villainAttack === "sword"){
         result = "You notice your opponent falter and unleash a vicious blast of magic towards him. He deftly dodges aside as the blast passes by him and swings his sword towards you, causing you a serious wound.";
         victor = "villain";
+        swordSound.play();
     }
 }
 
--- scripts/vendor.js
+++ scripts/vendor.js
@@ -3,6 +3,9 @@ const slaughterCount = document.getElementById("progress-score");
 const playerChoice = document.getElementById("player-choice");
 const villainChoice = document.getElementById("pc-choice");
 const musicPlayer = document.getElementById("music");
+const swordSound = document.getElementById("sword-sound");
+const shieldSound = document.getElementById("shield-sound");
+const wandSound = document.getElementById("wand-sound");
 let playerLives = 4;
 let villainLives = 4;
 let slaughtered = parseInt(slaughterCount.innerHTML);
@@ -15,6 +18,7 @@ function updateMoves(playerAttack, villainAttack){
 
     playerChoice.src= `/pictures/${playerAttack}.png`;
     villainChoice.src = `/pictures/${villainAttack}.png`;
+    villainChoice.style.transform = "scaleX(-1)"
 }
 
 function moreSlaughter(){
