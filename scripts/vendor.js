dc6e814c9f3059f844b062fb5c8684b510b29f1d
zerius182 <garryjoynes182@hotmail.co.uk>
2022-01-26 11:57:20 +0000

    Added various logic, scripts and functions

--- scripts/vendor.js
+++ scripts/vendor.js
@@ -2,10 +2,12 @@ const battleComm = document.getElementById("battle-commentary");
 const slaughterCount = document.getElementById("progress-score");
 const playerChoice = document.getElementById("player-choice");
 const villainChoice = document.getElementById("pc-choice");
-const musicPlayer = document.getElementById("music");
+const musicPlayerStart = document.getElementById("music-start");
 const swordSound = document.getElementById("sword-sound");
 const shieldSound = document.getElementById("shield-sound");
 const wandSound = document.getElementById("wand-sound");
+const healPot = document.getElementById("potion");
+
 let playerLives = 4;
 let villainLives = 4;
 let slaughtered = parseInt(slaughterCount.innerHTML);
@@ -28,13 +30,35 @@ function moreSlaughter(){
 
 function playerDamage(){
     let toRemove = document.getElementById(`player-life-${playerLives}`);
-    toRemove.remove()
+    toRemove.style.display = "none";
     playerLives -= 1;
 }
 
 function villainDamage(){
     let toRemove = document.getElementById(`villain-life-${villainLives}`);
-    toRemove.remove()
+    toRemove.style.display = "none";
     villainLives -= 1;
 }
 
+function healCharacters(){
+    playerLives = 4;
+    villainLives = 4;
+    healPot.style.display = "block";
+    for (let i=0; i<5; i+=1){
+        let playerAdd = document.getElementById(`player-life-${i}`);
+        let villainAdd = document.getElementById(`villain-life-${i}`);
+        playerAdd.style.display = "block";
+        villainAdd.style.display = "block";
+    }
+    console.log(playerLives, villainLives);    
+}
+
+function healPlayer(){
+    playerLives = 4;
+    for (let i=0; i<5; i+=1){
+        let playerAdd = document.getElementById(`player-life-${i}`);
+        playerAdd.style.display = "block";
+        healPot.style.display = "none";
+    }
+    console.log(playerLives, villainLives);
+}
